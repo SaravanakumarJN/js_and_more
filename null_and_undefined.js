3dd5cae008b4eaf58104efe8752240a65752d0bb
SaravanakumarJN <kumarjnsaravana@gmail.com>
2021-08-04 15:47:46 +0530

    null and undefined

---
+++ null_and_undefined.js
@@ -0,0 +1,9 @@
+/*
+null and undefined don't have any properties in them
+*/ 
+
+let null_check = null 
+console.log("Null type : ", typeof null_check)
+
+let undefined_check = undefined
+console.log("Undefined check : ", typeof undefined_check)
